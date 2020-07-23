81143ef8034afbda976879e233a7ada682e5d6aa
Siaoja <160692147@qq.com>
2020-07-23 12:04:56 +0800

    [Junit-TDD-practice][spike lu] feature: implement given 15 when count off game play then return FizzBuzz.

--- src/main/java/junit5/tdd/CountOffGame.java
+++ src/main/java/junit5/tdd/CountOffGame.java
@@ -3,6 +3,9 @@ package junit5.tdd;
 public class CountOffGame {
 
     public static String play(int number) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        }
         if (number % 3 == 0) {
             return "Fizz";
         }
