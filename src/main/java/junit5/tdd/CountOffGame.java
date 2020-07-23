03201fe113a035ffb817627144ed001952829dfc
Siaoja <1610692147@qq.com>
2020-07-23 11:55:30 +0800

    [Junit-TDD-practice][spike lu] feature: implement given 3 when count off game play then return Fizz.

--- src/main/java/junit5/tdd/CountOffGame.java
+++ src/main/java/junit5/tdd/CountOffGame.java
@@ -3,6 +3,9 @@ package junit5.tdd;
 public class CountOffGame {
 
     public static String play(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
